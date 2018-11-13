1b64e0924cc5608c6d6580ee0ea218d34e39f601
omrobbie <putu.roby@yahoo.com>
2018-11-13 16:16:09 +0700

    create listview with dummy data

--- 03_category_route/lib/category_route.dart
+++ 03_category_route/lib/category_route.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'category.dart';
+
+final _backgroundColor = Colors.green[100];
 
 class CategoryRoute extends StatelessWidget {
   const CategoryRoute();
@@ -25,10 +28,43 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final appBar = AppBar();
-    final listView = Container();
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        icon: Icons.cake,
+      ));
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
 
     return Scaffold(
       appBar: appBar,
