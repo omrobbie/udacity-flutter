5e9ec93b45c5149c5ff2b8b6d59585e2dab41883
omrobbie <putu.roby@yahoo.com>
2018-11-18 05:24:34 +0700

    create backdrop panel and title class

---
+++ 07_backdrop/lib/backdrop.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'category.dart';
+
+const double _kFlingVelocity = 2.0;
+
+class _BackdropPanel extends StatelessWidget {
+  final VoidCallback onTap;
+  final GestureDragUpdateCallback onVerticalDragUpdate;
+  final GestureDragEndCallback onVerticalDragEnd;
+  final Widget title;
+  final Widget child;
+
+  const _BackdropPanel({
+    Key key,
+    this.onTap,
+    this.onVerticalDragUpdate,
+    this.onVerticalDragEnd,
+    this.title,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 2.0,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(16.0),
+        topRight: Radius.circular(16.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onVerticalDragUpdate: onVerticalDragUpdate,
+            onVerticalDragEnd: onVerticalDragEnd,
+            onTap: onTap,
+            child: Container(
+              height: 48.0,
+              padding: EdgeInsetsDirectional.only(start: 16.0),
+              alignment: AlignmentDirectional.centerStart,
+              child: DefaultTextStyle(
+                style: Theme.of(context).textTheme.subhead,
+                child: title,
+              ),
+            ),
+          ),
+          Divider(
+            height: 1.0,
+          ),
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BackdropTitle extends AnimatedWidget {
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const _BackdropTitle ({
+    Key key,
+    Listenable listenable,
+    this.frontTitle,
+    this.backTitle,
+  }) : super(key: key, listenable: listenable);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = this.listenable;
+
+    return DefaultTextStyle(
+      style: Theme.of(context).primaryTextTheme.title,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
+      child: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: CurvedAnimation(
+              parent: ReverseAnimation(animation)
+              curve: Interval(0.5, 1.0),
+            ).value,
+            child: backTitle,
+          ),
+          Opacity(
+            opacity: CurvedAnimation(
+              parent: animation,
+              curve: Interval(0.5, 1.0),
+            ).value,
+            child: frontTitle,
+          ),
+        ],
+      ),
+    );
+  }
+}
