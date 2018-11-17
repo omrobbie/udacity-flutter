3f49cf720b1ebbf8bf16a12a2ff7049fc503aa1d
omrobbie <putu.roby@yahoo.com>
2018-11-18 04:32:11 +0700

    create category tile class

---
+++ 07_backdrop/lib/category_tile.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'category.dart';
+import 'unit_converter.dart';
+
+const _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+class CategoryTile extends StatelessWidget {
+  final Category category;
+  final ValueChanged<Category> onTap;
+
+  const CategoryTile({
+    Key key,
+    @required this.category,
+    @required this.onTap,
+  })  : assert(category != null),
+        assert(onTap != null),
+        super(key: key);
+
+  void _navigateToConverter(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 1.0,
+          title: Text(
+            category.name,
+            style: Theme.of(context).textTheme.display1,
+          ),
+          centerTitle: true,
+          backgroundColor: category.color,
+        ),
+        body: UnitConverter(category: category),
+        resizeToAvoidBottomPadding: false,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: category.color['hightlight'],
+          splashColor: category.color['splash'],
+          onTap: () => _navigateToConverter(context),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    category.icon,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    category.name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
