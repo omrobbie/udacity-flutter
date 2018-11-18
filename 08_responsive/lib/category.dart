d788714b7869e5c1ec6f40bd8d6474bd0623ba3b
omrobbie <putu.roby@yahoo.com>
2018-11-18 07:19:48 +0700

    create category class

---
+++ 08_responsive/lib/category.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'unit.dart';
+
+class Category {
+  final String name;
+  final ColorSwatch color;
+  final List<Unit> units;
+  final IconData icon;
+
+  const Category({
+    @required this.name,
+    @required this.color,
+    @required this.units,
+    @required this.icon,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(units != null),
+        assert(icon != null);
+}
