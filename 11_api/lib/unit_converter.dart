f64758e715f059d10cc7bff939933876d0417c9f
omrobbie <putu.roby@yahoo.com>
2018-11-21 06:27:30 +0700

    refactor: update conversion in unit converter class

--- 11_api/lib/unit_converter.dart
+++ 11_api/lib/unit_converter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'category.dart';
 import 'unit.dart';
+import 'api.dart';
 
 import 'dart:async';
 
@@ -95,12 +96,22 @@ class _UnitConverterState extends State<UnitConverter> {
     return outputNum;
   }
 
-  void _updateConversion() {
-    setState(() {
-      _convertedValue = _format(
-        _inputValue * (_toValue.conversion / _fromValue.conversion),
-      );
-    });
+  Future<void> _updateConversion() async {
+    if (widget.category.name == apiCategory['name']) {
+      final api = Api();
+      final conversion = await api.convert(apiCategory['route'],
+          _inputValue.toString(), _fromValue.name, _toValue.name);
+
+      setState(() {
+        _convertedValue = _format(conversion);
+      });
+    } else {
+      setState(() {
+        _convertedValue = _format(
+          _inputValue * (_toValue.conversion / _fromValue.conversion),
+        );
+      });
+    }
   }
 
   void _updateInputValue(String input) {
