1af5e822b3002b626f3bc9ee28cac2be344349cf
omrobbie <putu.roby@yahoo.com>
2018-11-21 06:54:44 +0700

    create category class

---
+++ 12_error/lib/category.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'unit.dart';
+
+class Category {
+  final String name;
+  final ColorSwatch color;
+  final List<Unit> units;
+  final String iconLocation;
+
+  const Category({
+    @required this.name,
+    @required this.color,
+    @required this.units,
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(units != null),
+        assert(iconLocation != null);
+}
