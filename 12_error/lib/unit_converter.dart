daf0902bb0d11a0d540392fc54d78b9831f40917
omrobbie <putu.roby@yahoo.com>
2018-11-21 07:14:26 +0700

    refactor: create error layout in unit converter

--- 12_error/lib/unit_converter.dart
+++ 12_error/lib/unit_converter.dart
@@ -26,6 +26,7 @@ class _UnitConverterState extends State<UnitConverter> {
   List<DropdownMenuItem> _unitMenuItems;
   bool _showValidationError = false;
   final _inputKey = GlobalKey(debugLabel: 'inputText');
+  bool _showErrorUI = false;
 
   @override
   void initState() {
@@ -80,6 +81,7 @@ class _UnitConverterState extends State<UnitConverter> {
 
     if (outputNum.contains('.') && outputNum.endsWith('0')) {
       var i = outputNum.length - 1;
+
       while (outputNum[i] == '0') {
         i -= 1;
       }
@@ -100,13 +102,20 @@ class _UnitConverterState extends State<UnitConverter> {
       final conversion = await api.convert(apiCategory['route'],
           _inputValue.toString(), _fromValue.name, _toValue.name);
 
-      setState(() {
-        _convertedValue = _format(conversion);
-      });
+      if (conversion == null) {
+        setState(() {
+          _showErrorUI = true;
+        });
+      } else {
+        setState(() {
+          _convertedValue = _format(conversion);
+        });
+      }
     } else {
       setState(() {
         _convertedValue = _format(
-            _inputValue * (_toValue.conversion / _fromValue.conversion));
+          _inputValue * (_toValue.conversion / _fromValue.conversion),
+        );
       });
     }
   }
@@ -190,6 +199,38 @@ class _UnitConverterState extends State<UnitConverter> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.category.units == null ||
+        (widget.category.name == apiCategory['name'] && _showErrorUI)) {
+      return SingleChildScrollView(
+        child: Container(
+          margin: _padding,
+          padding: _padding,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+            color: widget.category.color['error'],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline,
+                size: 180.0,
+                color: Colors.white,
+              ),
+              Text(
+                "Oh no! We can't connect right now!",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     final input = Padding(
       padding: _padding,
       child: Column(
